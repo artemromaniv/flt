76637f279cde6d494bdd0461e7ceb05e80f3ba3a
artem <artemromaniv@yahoo.com>
2023-01-17 09:47:34 -0800

    create copy of file before filtering, and create parameter that disables this feature

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,33 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // Get the command line arguments for the file path, specified strings and --nompt flag
+    // Get the command line arguments for the file path, specified strings and --drop flag
     let args: Vec<String> = env::args().collect();
-    
     let file_path = &args[1];
-
     let strings_to_remove: Vec<&str> = args[2..args
         .iter()
-        .position(|x| x == "--nompt")
+        .position(|x| x == "--drop")
         .unwrap_or(args.len())]
         .iter()
         .map(|x| x.as_str())
         .collect();
-
-    let remove_empty_lines = args.contains(&"--nompt".to_string());
+    let create_backup = !args.contains(&"--drop".to_string());
 
     // Read the contents of the .txt file
     let file_contents = fs::read_to_string(file_path).expect("Error reading file");
 
+    //create a copy of the original file if --drop is not passed
+    if create_backup {
+        fs::copy(file_path, format!("{}.backup", file_path)).expect("Error creating backup file");
+    }
+
     // Split the file contents into lines
     let lines: Vec<&str> = file_contents.split("\n").collect();
 
-    // Iterate through the lines and remove those that contain the specified strings and/or are empty
+    // Iterate through the lines and remove those that contain the specified strings
     let new_lines: Vec<&str> = lines
         .into_iter()
-        .filter(|x| {
-            !strings_to_remove.iter().any(|y| x.contains(y))
-                && (!remove_empty_lines || x.trim() != "")
-        })
+        .filter(|x| !strings_to_remove.iter().any(|y| x.contains(y)))
         .collect();
 
     // Join the remaining lines into a single string
